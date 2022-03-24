17a2aa1e026428c983f93e66c530c6f77d272ac2
ImoTechs <adzembehj@gmail.com>
2022-03-24 06:07:54 -0700

    heroku first

--- models/settings.py
+++ models/settings.py
@@ -80,7 +80,10 @@ WSGI_APPLICATION = 'models.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default':dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -122,7 +125,6 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
